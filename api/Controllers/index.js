65c8cf35bf05ab274781f7c7e49469db90e346cb
Gwerh <lalo@perronegro.cl>
2022-12-15 18:20:47 -0300

    Orden preliminar

--- api/Controllers/index.js
+++ api/Controllers/index.js
@@ -1,5 +1,12 @@
 const {mediaList} = require('../misc/images/index.js')
 const {talesList} = require('../misc/sounds/index.js')
+/*-----------------World of Gwerh-----------------*/ 
+
+function getTales(){
+    return talesList
+}
+
+/*-----------------La Ruina TV-----------------*/ 
 
 function addMedia(el){
     if(!mediaList.includes(el)){
@@ -12,10 +19,6 @@ function addMedia(el){
 
 function getMedia(){
     return mediaList
-}
-
-function getTales(){
-    return talesList
 }
 
 module.exports = {addMedia, getMedia, getTales}
